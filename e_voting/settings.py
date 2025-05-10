5206420bb525a2f10b2c76cd50aefe174f7d9ea9
BlessingAllison <your-email@example.com>
2025-05-10 14:58:51 -0700

    Update static files configuration and bower installation

--- e_voting/settings.py
+++ e_voting/settings.py
@@ -82,6 +82,18 @@ MIDDLEWARE = [
     'account.middleware.AccountCheckMiddleWare',
 ]
 
+if not DEBUG:
+    MIDDLEWARE = [
+        'django.middleware.security.SecurityMiddleware',
+        'whitenoise.middleware.WhiteNoiseMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
+
 ROOT_URLCONF = 'e_voting.urls'
 
 TEMPLATES = [
